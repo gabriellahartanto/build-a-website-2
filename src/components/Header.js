920df75f69711abdcebaa08f8782cc51bc98a78b
gabriellahartanto <gabriella.hartanto@icloud.com>
2020-12-29 14:31:24 +0700

    add router and fix header cities

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
         <h1> TODO </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=Jakarta"> Jakarta </a>
+        <a href="/?city=New%20York"> New York </a>
+        <a href="/?city=Tokyo"> Tokyo </a>
+        <a href="/?city=Sydney"> Sydney </a>
       </nav>
     </header>
   );
